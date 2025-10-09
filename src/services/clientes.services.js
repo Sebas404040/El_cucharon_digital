2ad2c3fedb8114a832646ae9dcd00b2fc6d11b1c
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-10-09 18:51:38 -0500

    feat: :bug: Incorrect params
    params fixed succesfully
    
    none

--- src/services/clientes.services.js
+++ src/services/clientes.services.js
@@ -1,38 +1,43 @@
-import database from "../database/connectionDB";
+import database from "../database/connectionDB.js";
 
 const COLECCION_CLIENTES = "clientes";
 
 export async function crearCliente(datos) {
     const {nombre, email, telefono, fechaRegistro, activo} = datos
-    const jugador = {
+    const cliente = {
         nombre,
         email,
         telefono,
         fechaRegistro,
         activo
     }
-    await database.obtenerBD().collection(COLECCION_CLIENTES).insertOne(jugador);
+    const collection = await database.getCollection(COLECCION_CLIENTES);
+    await collection.insertOne(cliente);
     return {Message: "Cliente creado con exito"}
 }
 
 export async function obtenerClientes() {
-    return await database.obtenerBD().collection(COLECCION_CLIENTES).find().toArray();
+    const collection = await database.getCollection(COLECCION_CLIENTES);
+    return await collection.find().toArray();
 }
 
 export async function obtenerCliente(nombre) {
-    const newId = await obtenerBD().collection(COLECCION_CLIENTES).findOne({nombre}, {_id: 1}).id;
-    return await obtenerBD().collection(COLECCION_CLIENTES).findOne({newId});
+    const collection = await database.getCollection(COLECCION_CLIENTES);
+    const cliente = await collection.findOne({nombre: nombre});
+    if (!cliente) throw new Error("Cliente no encontrado");
+    return cliente;
 }
 
-export async function actualizarCliente(id, datos) {
-    const {nombre, email, telefono, fechaRegistro, activo} = datos
-    const resultado = await database.obtenerBD().collection(COLECCION_CLIENTES).updateOne({id}, {$set: {datos}});
-    if(resultado.matchedCount === 0) throw new Error("Jugador no encontrado!!!");
-    return {Message: "Jugador actualizado con exito"}
+export async function actualizarCliente(nombre, datos) {
+    const collection = await database.getCollection(COLECCION_CLIENTES);
+    const resultado = await collection.updateOne({nombre: nombre}, {$set: datos});
+    if(resultado.matchedCount === 0) throw new Error("Cliente no encontrado");
+    return {Message: "Cliente actualizado con exito"}
 }
 
-export async function eliminarCliente(id) {
-    const resultado = await obtenerBD().collection(COLECCION_CLIENTES).deleteOne({id});
-    if (resultado.deletedCount === 0) throw new Error("Jugador no encontrado!!!");
-    return {message: "Jugador eliminado!!!"};
+export async function eliminarCliente(nombre) {
+    const collection = await database.getCollection(COLECCION_CLIENTES);
+    const resultado = await collection.deleteOne({nombre: nombre});
+    if (resultado.deletedCount === 0) throw new Error("Cliente no encontrado");
+    return {message: "Cliente eliminado con exito"};
 }
