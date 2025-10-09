02ae65e72f0601137afec5e7186a03a8979cbc50
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-10-09 18:52:16 -0500

    feat: :sparkles: Route placed
    route with controllers added
    
    none

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,20 +1,23 @@
 import {Router} from 'express';
 
-const router = Router();
+const routeClientes = Router();
 
 import {
-    crearCliente,
-    obtenerClientes,
-    obtenerCliente,
-    actualizarCliente,
-    eliminarCliente
-} from "../services/clientes.services";
-
-import { crearCliente_DTO, actualizarCliente_DTO } from '../DTOS/clientesDTO';
-
-import { crearCliente_DTO } from '../DTOS/clientesDTO';
-import { ValidationDTO } from '../middlewares/validationDTO';
+    obtenerClientes_controller,
+    obtenerCliente_controller,
+    crearCliente_controller,
+    actualizarCliente_controller,
+    eliminarCliente_controller
+} from '../controllers/clientes.controller.js';
 
 
-router.post("/", crearCliente_DTO, ValidationDTO)
+import { crearCliente_DTO, actualizarCliente_DTO } from '../DTOS/clientesDTO.js';
+import { ValidationDTO } from '../middlewares/validationDTO.js';
 
+routeClientes.get('/', obtenerClientes_controller);
+routeClientes.get('/:nombre', obtenerCliente_controller);
+routeClientes.post('/', crearCliente_DTO, ValidationDTO, crearCliente_controller);
+routeClientes.put('/:nombre', actualizarCliente_DTO, ValidationDTO, actualizarCliente_controller);
+routeClientes.delete('/:nombre', eliminarCliente_controller);
+
+export default routeClientes;
