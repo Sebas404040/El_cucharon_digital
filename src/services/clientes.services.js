4df17a7b92ca9a6aaa305c6ebdcbef0276cd5b5a
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-10-09 08:49:54 -0500

    feat: :sparkles: Client functionalities
    Functionalities develoed succesfully
    
    none

--- src/services/clientes.services.js
+++ src/services/clientes.services.js
@@ -2,10 +2,37 @@ import database from "../database/connectionDB";
 
 const COLECCION_CLIENTES = "clientes";
 
+export async function crearCliente(datos) {
+    const {nombre, email, telefono, fechaRegistro, activo} = datos
+    const jugador = {
+        nombre,
+        email,
+        telefono,
+        fechaRegistro,
+        activo
+    }
+    await database.obtenerBD().collection(COLECCION_CLIENTES).insertOne(jugador);
+    return {Message: "Cliente creado con exito"}
+}
+
 export async function obtenerClientes() {
     return await database.obtenerBD().collection(COLECCION_CLIENTES).find().toArray();
 }
 
-export async function obtenerCliente(id) {
-    return await obtenerBD().collection(COLECCION_CLIENTES).findOne({id});
+export async function obtenerCliente(nombre) {
+    const newId = await obtenerBD().collection(COLECCION_CLIENTES).findOne({nombre}, {_id: 1}).id;
+    return await obtenerBD().collection(COLECCION_CLIENTES).findOne({newId});
+}
+
+export async function actualizarCliente(id, datos) {
+    const {nombre, email, telefono, fechaRegistro, activo} = datos
+    const resultado = await database.obtenerBD().collection(COLECCION_CLIENTES).updateOne({id}, {$set: {datos}});
+    if(resultado.matchedCount === 0) throw new Error("Jugador no encontrado!!!");
+    return {Message: "Jugador actualizado con exito"}
+}
+
+export async function eliminarCliente(id) {
+    const resultado = await obtenerBD().collection(COLECCION_CLIENTES).deleteOne({id});
+    if (resultado.deletedCount === 0) throw new Error("Jugador no encontrado!!!");
+    return {message: "Jugador eliminado!!!"};
 }
