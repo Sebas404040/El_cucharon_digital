93e9d08bfd65530fda795391c16e7a4622b3ccf9
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-10-09 23:24:57 -0500

    feat: :hammer: DTO object
    validations created taking into consideration the logic
    
    none

--- src/DTOS/clientesDTO.js
+++ src/DTOS/clientesDTO.js
@@ -10,7 +10,6 @@ export const crearCliente_DTO = [
 
 export const actualizarCliente_DTO = [
     param("nombre").isString().notEmpty().withMessage("El nombre en la URL no puede estar vacío"),
-    body("nombre").optional().isString().trim().notEmpty().withMessage("Nombre no valido"),
     body("email").optional().isEmail().trim().notEmpty().withMessage("Email invalido"),
     body("telefono").optional().isString().trim().notEmpty().isLength({min: 10, max: 15}).withMessage("Telefono invalido"),
     body("fechaRegistro").optional().isISO8601().toDate().withMessage("Fecha invalida, debe estar en formato YYYY-MM-DD"),
