78d9e43570b9636756a3813bd98383adebd30fd0
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-10-10 01:36:20 -0500

    feat: :zap: recipes route updated
    modified for ingredients functionalities
    
    nojne

--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -8,10 +8,15 @@ import {
     obtenerReceta_controller,
     actualizarReceta_controller,
     eliminarReceta_controller,
-    obtenerRecetasPorCliente_controller
+    obtenerRecetasPorCliente_controller,
+    agregarIngredientes_controller,
+    verIngredientes_controller,
+    eliminarIngrediente_controller,
+    buscarIngredientePorReceta_controller,
+    buscarRecetasPorIngrediente_controller
 } from "../controllers/recetas.controller.js";
 
-import { crearReceta_DTO, actualizarReceta_DTO } from "../DTOS/recetasDTO.js";
+import { crearReceta_DTO, actualizarReceta_DTO, agregarIngredientes_DTO } from "../DTOS/recetasDTO.js";
 import { ValidationDTO } from "../middlewares/validationDTO.js";
 
 routeRecetas.post("/", crearReceta_DTO, ValidationDTO, crearReceta_controller);
@@ -19,6 +24,12 @@ routeRecetas.get("/", obtenerRecetas_controller);
 routeRecetas.get("/:nombre", obtenerReceta_controller);
 routeRecetas.patch("/:nombre", actualizarReceta_DTO, ValidationDTO, actualizarReceta_controller);
 routeRecetas.delete("/:nombre", eliminarReceta_controller);
+routeRecetas.patch("/:nombre/ingredientes", agregarIngredientes_DTO, ValidationDTO, agregarIngredientes_controller);
 routeRecetas.get("/cliente/:id_cliente", obtenerRecetasPorCliente_controller);
+routeRecetas.get("/:nombre/ingredientes", verIngredientes_controller);
+routeRecetas.delete("/:nombre/ingredientes/:nombreIngrediente", eliminarIngrediente_controller);
+routeRecetas.get("/:nombre/ingredientes/:nombreIngrediente", buscarIngredientePorReceta_controller);
+routeRecetas.get("/ingrediente/:nombreIngrediente", buscarRecetasPorIngrediente_controller);
+
 
 export default routeRecetas;
